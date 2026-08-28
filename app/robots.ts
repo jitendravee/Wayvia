1946914da40da3c4bbf3e7e900a8a974bb44c486
jitendravee <jitendravee098@gmail.com>
2026-08-28 13:05:03 +0530

    added the fixes robot

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/"],
     },
-    sitemap: "https://wayvia.com/sitemap.xml",
+    sitemap: "https://www.wayvia.xyz/sitemap.xml",
   };
 }
